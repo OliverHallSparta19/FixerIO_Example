70045ada3eb1d1d48bb756deb6d5b81f3deb0479
Oliver <OHall@spartaglobal.com>
2018-11-07 13:45:38 +0000

    commiting lunch time fixes

--- src/main/java/FixerIO_Example/App.java
+++ src/main/java/FixerIO_Example/App.java
@@ -6,6 +6,9 @@ public class App
     public static void main(String[] args) {
         KeyReader keyReader = new KeyReader();
         System.out.println(keyReader.getApi_key());
+        FixerHTTPManager manager = new FixerHTTPManager();
+        manager.setLatestRates();
+        System.out.println(manager.getLatestRates());
     }
 
 }
